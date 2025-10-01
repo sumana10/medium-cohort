c72c79ef0c3cea5948870818593a49f69b85588a
sumana10 <sumanaonlyme@gmail.com>
2025-10-01 10:13:38 +0530

    add updated prisma

--- frontend/src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Appbar from "./Appbar";
-import Auth from "./Auth";
-import BlogCard from "./BlogCard";
-import Quote from "./Quote";
-import Shimmer from "./Shimmer";
-import Spinner from "./Spinner";
-import SingleBlog from "./SingleBlog";
-
-export { Appbar, Auth, BlogCard, Quote, Shimmer, Spinner, SingleBlog }
--- frontend/src/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import Signin from "./Signin";
-import Signup from "./Signup";
-import Blog from "./Blog";
-import Blogs from "./Blogs";
-import Publish from "./Publish"
-
-
-export { Signin, Signup, Blog, Blogs, Publish }
